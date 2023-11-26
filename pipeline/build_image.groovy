8e5fb6c2f198e7f646ae4427dde0b32e1bba31e1
Administrator <admin@example.com>
2023-11-26 14:13:02 +0000

    Update build_image.groovy

--- pipeline/build_image.groovy
+++ pipeline/build_image.groovy
@@ -1,6 +1,12 @@
 pipeline {
     agent any
     stages {
+        stage('Test docker command') {
+            steps {
+                docker ps
+                echo 'Login successfully!'
+            }
+        }
         stage('Login Docker Hub') {
             steps {
                 docker login --password-stdin -u 'xnylesx' -p 'Strator@202301'
