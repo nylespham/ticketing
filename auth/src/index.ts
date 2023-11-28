da983e42d7f62ec1e13318dd2d2b4f166d9e3c8d
Huy Pham <nylespham@Huys-MacBook-Air.local>
2023-11-28 18:08:24 +0700

    Ingress modified

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
-import {json} from "body-parser";
+import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current_user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const port = 3000;
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
     console.log(`Listening on port ${port}`);
