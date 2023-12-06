c88a2eba47bf201dfc4b4e73c06f0ab6eacf29c1
Huy Pham <nylespham@Huys-MacBook-Air.local>
2023-12-06 09:59:09 +0700

    Error handling done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import "express-async-errors";
 
 const port = 3000;
 const app = express();
@@ -14,7 +16,13 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-app.use(errorHandler)
+
+app.all("*", async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log(`Listening on port ${port}`);
 });
